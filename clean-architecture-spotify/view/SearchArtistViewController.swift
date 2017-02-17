ce8c58f361e0e1063340a1840ec6dc2fb24d4fe6
Ivan Alvarez <ivan.alvarez@schibsted.com.mx>
2017-02-17 16:55:38 -0600

    Initialize delegates

--- clean-architecture-spotify/view/SearchArtistViewController.swift
+++ clean-architecture-spotify/view/SearchArtistViewController.swift
@@ -1,9 +1,6 @@
 import UIKit
 
-class SearchViewController: UIViewController, ArtistsUI {
-
-    @IBOutlet weak var searchArtistInput: UITextField!
-    @IBOutlet weak var searchArtistButton: UIButton!
+class SearchArtistViewController: UIViewController, ArtistsUI {
 
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
@@ -35,16 +32,9 @@ class SearchViewController: UIViewController, ArtistsUI {
     func showEmptyCase() {
         // show Empty Case
     }
-
-    @IBAction func onSearchArtist(_: Any) {
-        let artistName: String = searchArtistInput.text!
-        artistPresenter?.searchArtist(query: artistName)
-        artistPresenter?.viewDidLoad()
-    }
-
 }
 
-extension SearchViewController: UISearchBarDelegate {
+extension SearchArtistViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let artistName: String = searchBar.text!
         print("The search text is: '\(searchBar.text!)'")
@@ -54,16 +44,16 @@ extension SearchViewController: UISearchBarDelegate {
     }
 }
 
-extension SearchViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension SearchArtistViewController: UITableViewDataSource {
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
         return 0
     }
-    
+
 }
 
-extension SearchViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+extension SearchArtistViewController: UITableViewDelegate {
+    func tableView(_: UITableView, cellForRowAt _: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
-    
+
 }
