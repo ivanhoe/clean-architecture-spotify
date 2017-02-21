5d0376516d2762b69e6083688ed4b28c672b83dd
Ivan Alvarez <ivan.alvarez@schibsted.com.mx>
2017-02-20 21:56:17 -0600

    Removing pod files

--- Pods/Target Support Files/Pods-clean-architecture-spotify/Pods-clean-architecture-spotify-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_clean_architecture_spotify : NSObject
-@end
-@implementation PodsDummy_Pods_clean_architecture_spotify
-@end
--- Pods/Target Support Files/Pods-clean-architecture-spotifyTests/Pods-clean-architecture-spotifyTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_clean_architecture_spotifyTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_clean_architecture_spotifyTestsVersionString[];
-
