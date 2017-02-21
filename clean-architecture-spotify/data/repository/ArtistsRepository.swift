77b702a0266f734f18d451c8f67cca774f8f1025
Ivan Alvarez <ivan.alvarez@schibsted.com.mx>
2017-02-20 22:01:00 -0600

    Mapping entity to domain

--- clean-architecture-spotify/data/repository/ArtistsRepository.swift
+++ clean-architecture-spotify/data/repository/ArtistsRepository.swift
@@ -4,49 +4,46 @@ import Foundation
 struct ArtistsRepository<T: Datasource>: Repository where T.T == ArtistItemEntity {
     
     let datasource: T
+    let defaultImage: String = "http://bit.ly/swiftDefaultLogo"
     
     func getAll(at query: String, completion: @escaping ([Artist]) -> Void) {
-        datasource.getAll(at: query) { artistsItemEntity in
+        
+        datasource.getAll(at: query) { entities in
             
-            // TODO: transform ArtistsEntityList to Artists            
-            var items:[Artist] = []
-            for item in artistsItemEntity {
-                let value = NewArtist(name:item.name,popularity:String(item.popularity),followers:String(item.followers.total),image:"")
-                items.append(value)
+            var artists: [Artist] = []
+            for entity in entities {
+                artists.append(self.createArtist(entity: entity))
             }
             
-            completion(items)
+            completion(artists)
         }
     }
-
-    func get(at name: String, completion: @escaping (Artist) -> Void) {
+    
+    func get(at name: String, completion _: @escaping (Artist) -> Void) {
         datasource.get(at: name) { _ in
             // TODO: transform ArtistsEntity to Artist
             // completion(artist)
         }
     }
+    
+    func createArtist(entity: ArtistItemEntity) -> Artist {
+        
+        var artist: NewArtist = NewArtist(name: "", popularity: "", followers: "", imageURL: "")
+        
+        if entity.images.isEmpty {
+            artist = NewArtist(name: entity.name,
+                               popularity: String(entity.popularity),
+                               followers: String(entity.followers.total),
+                               imageURL: self.defaultImage)
+        } else {
+            let image = entity.images[0]
+            artist = NewArtist(name: entity.name,
+                               popularity: String(entity.popularity),
+                               followers: String(entity.followers.total),
+                               imageURL: image.url)
+        }
+        
+        return artist
+    }
+    
 }
-
-
-/*
- 
- func get(at name: String, completion: @escaping (ArtistEntityItem) -> Void) {
- let artist = self.artists.filter { $0.name == name }.first!
- completion(artist)
- }
- 
- func getAll(at query: String, completion: @escaping ([ArtistEntityItem]) -> Void) {
- Alamofire
- .request("https://api.spotify.com/v1/search?type=artist&q=\(query)")
- .responseJSON { response in
- let json = response.result.value as? [String: Any]
- //                print(json)
- // TODO: PARSING JSON to return artists list
- // self.artists = self.getFakeArtistList()
- let result = ArtistEntityResponse(fromDictionary: json!)
- self.artists = result.artists.items
- 
- completion(self.artists)
- }
- }
-*/
