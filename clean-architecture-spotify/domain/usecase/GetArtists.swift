f650eda7edfb702118a61797e5c976f5fe01533b
Ivan Alvarez <ivan.alvarez@schibsted.com.mx>
2017-02-20 22:02:06 -0600

    removing print

--- clean-architecture-spotify/domain/usecase/GetArtists.swift
+++ clean-architecture-spotify/domain/usecase/GetArtists.swift
@@ -4,7 +4,6 @@ struct GetArtists {
 
     func execute(at query: String, completion: @escaping ([Artist]) -> Void) {
         repository.getAll(at: query) { artists in
-            print("Until its ok")
             completion(artists)
         }
     }
